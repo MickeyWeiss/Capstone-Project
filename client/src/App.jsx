c969b114c6a910b24cee07ce0b1ba2c69ab2658a
Mickey Weiss <mickeyweiss@Mickeys-MacBook-Pro.local>
2024-09-24 12:02:24 -0600

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import NavBar from './components/Navigations'
 
 function App() {
 
   return (
     <>
-
+    <Router>
+      <div id = "NavBar">
+        <NavBar></NavBar>
+      </div>
+    </Router>
     </>
   )
 }
