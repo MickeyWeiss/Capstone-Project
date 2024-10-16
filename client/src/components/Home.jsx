3af0556f974782e9261045b1c20a0fdf52fbd706
Mickey Weiss <mickeyweiss@Mickeys-MacBook-Pro.local>
2024-10-15 20:26:21 -0600

    added header to home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react'
 const Home = () => {
     return (
         <>
+        <h1>Mickey Weiss' Fullstack Academy Captsone Travel Review Website</h1>
         <div id="welcome">
             <p>Welcome to my travel review website. Here you will be able to register and login to an account, read, rate and post reviews of varioius locations you and others have visited as well as delete reviews that you have created.</p>
 
